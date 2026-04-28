a131521175551b126c9d995738f8efb566a7e872
A Student <student@nowhere.edu>
2026-04-28 22:27:24 +0000

    Apr 28, 2026, 3:27 PM

--- tests.cpp
+++ tests.cpp
@@ -43,7 +43,7 @@ double getShippingCost(int shippingMethod, double orderCost){
    }
       if (shippingMethod == 2){
       if (orderCost <= 300){
-         return 10 + (300 * 0.02);
+         return 10 + (orderCost * 0.02);
       }
       else {
          return 0;
